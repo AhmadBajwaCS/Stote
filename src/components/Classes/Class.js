e2cb2559837da7799efde9ef9ed443f3f0062b6d
Christy <christyjlee07@gmail.com>
2023-01-09 19:48:10 -0600

    Set up class component and overview of classes

---
+++ src/components/Classes/Class.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+
+const Class = (props) => {
+    return(
+        <TouchableOpacity>
+            
+            <View>
+
+            </View>
+        </TouchableOpacity>
+    );
+};
+
+Class.defaultProps = {
+
+}
+
+
+const styles = StyleSheet.create({
+
+});
---
+++ src/screens/ClassesOverview/ClassesOverview.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    useColorScheme,
+  } from 'react-native';
+import Class from './src/components/Classes';
+
+//List classes
+//Add class button
