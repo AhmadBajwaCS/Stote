a762b606f5fa86d99ba96c14dbcdebca30c275f3
Desh Padmakumar <deshspo@gmail.com>
2023-01-12 09:37:08 -0800

    deleted const color

--- src/components/ClassComponent/ClassComponent.js
+++ src/components/ClassComponent/ClassComponent.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, } from "react-native";
 import { MaterialIcons } from '@expo/vector-icons';
 
-const color = ['#282828', '#C1FFB7', '#C7FBFF', '#FFE0CE', '#FFEEB4'];
-
-
 const ClassComponent = (props) => {
     return(
         <View style={styles.item}>
@@ -12,9 +9,7 @@ const ClassComponent = (props) => {
                 <Text style={styles.itemText}>{props.text}</Text>
             </TouchableOpacity>
         </View>
-        
     )
-    
 }
 
 const styles = StyleSheet.create({
@@ -27,13 +22,11 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         backgroundColor: '#C7FBFF',
         flexDirection: 'row',
-
     },
+
     itemText:{
         maxWidth: 80,
-
     },
-
     });
 
 export default ClassComponent;
