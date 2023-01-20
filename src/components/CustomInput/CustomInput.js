89276d87d848d836bc1971ec22eadfe2c3ef86c1
Desh Padmakumar <deshspo@gmail.com>
2023-01-19 22:12:56 -0800

    added dependency in customInput

--- src/components/CustomInput/CustomInput.js
+++ src/components/CustomInput/CustomInput.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TextInput, StyleSheet} from 'react-native';
+import { View, Text, TextInput, StyleSheet, keyboardType} from 'react-native';
 
 
 const CustomInput = ({value, setValue, placeholder, secureTextEntry, margVer}) => {
